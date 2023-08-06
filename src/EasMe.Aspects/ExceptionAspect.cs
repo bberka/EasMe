e1386a3e5e3ec87fa1ecd6dbe9326b28d5a3c69d
bberka <berkaybyr@icloud.com>
2023-08-06 17:03:55 +0300

    added EasMe.Aspects

---
+++ src/EasMe.Aspects/ExceptionAspect.cs
@@ -0,0 +1,46 @@
+﻿using AspectInjector.Broker;
+using Serilog;
+
+namespace EasMe.Aspects;
+
+/// <summary>
+/// Catches exceptions and re-throws them with a more detailed message.
+///
+/// 
+/// </summary>
+[Aspect(Scope.PerInstance)]
+[Injection(typeof(ExceptionAspect))]
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+public class ExceptionAspect : Attribute
+{
+   private readonly bool _enablelogging;
+   public ExceptionAspect(bool enablelogging)
+   {
+      _enablelogging = enablelogging;
+   }
+   public ExceptionAspect()
+   {
+      _enablelogging = true;
+   }
+   [Advice(Kind.Around)]
+   public object Around(
+      [Argument(Source.Target)] Func<object[], object> target,
+      [Argument(Source.Arguments)] object[] args,
+      [Argument(Source.Name)] string methodName,
+      [Argument(Source.Type)] Type type,
+      [Argument(Source.ReturnType)] Type returnType)
+   {
+      var className = type.Name;
+      var argList = string.Join(",", args);
+      try {
+         return target(args);
+      }
+      catch (Exception ex) {
+         var msg = $"Exception in {className}.{methodName}({argList}) Message:{ex.Message}";
+         if (_enablelogging)
+            Log.Fatal(ex, msg);
+         throw new Exception(msg, ex);
+      }
+   }
+
+}
