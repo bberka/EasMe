cdf9a58ed87a6ab6588bfe843a7d2ff00b9411df
bberka <berkaybyr@icloud.com>
2023-08-06 17:09:31 +0300

    exception type converter to result added (not-tested)

---
+++ src/EasMe.Aspects/ExceptionToResultConverterAspect.cs
@@ -0,0 +1,49 @@
+﻿using AspectInjector.Broker;
+using EasMe.Result;
+using Serilog;
+
+namespace EasMe.Aspects;
+
+/// <summary>
+/// Catches exceptions and converts them to Result Type
+///
+/// 
+/// </summary>
+[Aspect(Scope.PerInstance)]
+[Injection(typeof(ExceptionToResultConverterAspect))]
+[AttributeUsage(AttributeTargets.Method | AttributeTargets.Class)]
+public class ExceptionToResultConverterAspect : Attribute
+{
+   private readonly bool _enablelogging;
+   public ExceptionToResultConverterAspect(bool enablelogging)
+   {
+      _enablelogging = enablelogging;
+   }
+   public ExceptionToResultConverterAspect()
+   {
+      _enablelogging = true;
+   }
+   [Advice(Kind.Around)]
+   public object Around(
+      [Argument(Source.Target)] Func<object[], object> target,
+      [Argument(Source.Arguments)] object[] args,
+      [Argument(Source.Name)] string methodName,
+      [Argument(Source.Type)] Type type,
+      [Argument(Source.ReturnType)] Type returnType)
+   {
+      var className = type.Name;
+      var argList = string.Join(",", args);
+      try {
+         return target(args);
+      }
+      catch (Exception ex) {
+         var isResultType = returnType == typeof(Result.Result) || returnType == typeof(Result.ResultData<>);
+         if(!isResultType) throw;
+         return Result.Result.Exception(ex,"Exception",new []
+         {
+            new Param("methodName",methodName),
+         });
+      }
+   }
+
+}
