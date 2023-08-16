e552cadab3347cced7b289f135687e20e580782a
bberka <berkaybyr@icloud.com>
2023-08-16 15:41:18 +0300

    v3.0.1 release EasMe.Result

--- src/EasMe.Result/obj/Release/net6.0/EasMe.Result.AssemblyInfo.cs
+++ src/EasMe.Result/obj/Release/net6.0/EasMe.Result.AssemblyInfo.cs
@@ -13,11 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bberka")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Simple Lightweight Result type provider library with various helpul methods.")]
-[assembly: System.Reflection.AssemblyFileVersionAttribute("3.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("3.0.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("3.0.1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("3.0.1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasMe.Result")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasMe.Result")]
-[assembly: System.Reflection.AssemblyVersionAttribute("3.0.0")]
+[assembly: System.Reflection.AssemblyVersionAttribute("3.0.1")]
 [assembly: System.Reflection.AssemblyMetadataAttribute("RepositoryUrl", "https://github.com/bberka/EasMe")]
 
 // MSBuild WriteCodeFragment sınıfı tarafından oluşturuldu.
